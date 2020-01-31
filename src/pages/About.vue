2e9769240fd8d7c89fb59acaa222ac94f23e946e
Hans Pagel <hans.pagel@ueber.io>
2020-01-31 16:18:59 +0100

    add mailcoach form

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,19 @@
   <Layout>
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-    asdasd
+    <form
+      method="POST"
+      action="https://mailing.ueberdosis.io/mailcoach/subscribe/b4741e9b-69b7-4ba3-8828-4500236686fe"
+    >
+      <div>
+        <label for="email">Email</label>
+        <input name="email" />
+      </div>
+
+      <div>
+        <button type="submit">Subscribe</button>
+      </div>
+    </form>
   </Layout>
 </template>
 
