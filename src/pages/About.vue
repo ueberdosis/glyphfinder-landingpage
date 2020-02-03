bcdaa1d684a00227c46e97a3b2d9a554962b3020
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-03 11:22:27 +0100

    add axios

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,10 +2,7 @@
   <Layout>
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-    <form
-      method="POST"
-      action="https://mailing.ueberdosis.io/mailcoach/subscribe/b4741e9b-69b7-4ba3-8828-4500236686fe"
-    >
+    <form @submit.prevent="onSubmit">
       <div>
         <label for="email">Email</label>
         <input name="email" />
@@ -19,9 +16,24 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   metaInfo: {
     title: 'About us'
-  }
+  },
+
+  methods: {
+    onSubmit() {
+      axios
+        .post('https://mailing.ueberdosis.io/mailcoach/subscribe/b4741e9b-69b7-4ba3-8828-4500236686fe')
+        .then(response => {
+          console.log({response})
+        })
+        .catch(error => {
+          console.log({error})
+        })
+    },
+  },
 }
 </script>
