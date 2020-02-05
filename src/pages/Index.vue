2af51445dd7899e1f929652077df404feee002fe
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-05 20:18:06 +0100

    refactoring

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,8 +3,6 @@
     <app-section color="red" width="full">
       <banner>
         <h1>Don’t miss that glyph again.</h1>
-        <!-- <floating :glyphs="glyphs"/> -->
-        <!-- <floating2 /> -->
       </banner>
     </app-section>
   </Layout>
@@ -13,30 +11,11 @@
 <script>
 import Banner from '@/components/Banner'
 import AppSection from '@/components/AppSection'
-// import Floating2 from '@/components/Floating2'
 
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  },
-
   components: {
     Banner,
     AppSection,
-    // Floating2
   },
-
-  data() {
-    return {
-      glyphs: ['⟵', '⟶', '⇐', '⇒', '⬆', '⌘', '♥', '☀', '€', '¥', '‰', '⁋', '≈', '×', '©', '‡', '⅟', '⅞', '∞', '˚', '℮', '∑', '↳', '⚠', '⌥', '¤', '⌧', '⌫', '™', '⁆', '—', '÷', '…', '∫', '◊', '∅', '⎋', '↪',]
-      // glyphs: ['✌️', '🙃', '😍', '🤪', '🤓', '🤯', '💩', '👌', '🖖', '👀', '🙅‍♂️', '🐱', '🐝']
-    }
-  }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
