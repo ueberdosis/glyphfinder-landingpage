aafad74b74f0ce8ff786562d31a4778ca88915c9
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-07 15:02:17 +0100

    improve form

--- src/components/NewsletterForm/index.vue
+++ src/components/NewsletterForm/index.vue
@@ -25,28 +25,77 @@
         </div>
       </form>
     </template>
-  </mailchimp-subscribe> -->
-  <div></div>
+  </mailchimp-subscribe>
+  <div></div> -->
+  <form class="newsletter-form" @submit.prevent="onSubmit">
+    <div class="newsletter-form__content">
+      <input
+        class="newsletter-form__input"
+        type="email"
+        v-model="email"
+        placeholder="Your email address"
+      >
+      <button class="newsletter-form__button" :disabled="loading" type="submit">
+        Subscribe
+      </button>
+    </div>
+    <div class="newsletter-form__error" v-if="error">
+      {{ error }}
+    </div>
+    <div class="newsletter-form__success" v-if="success">
+      To complete the subscription process, please click the link in the email we just sent you.
+    </div>
+  </form>
 </template>
 
 <script>
 // import MailchimpSubscribe from 'vue-mailchimp-subscribe'
+import axios from 'axios'
 
 export default {
   components: {
     // MailchimpSubscribe,
   },
 
-  methods: {
-    onSuccess() {
-      this.email = null
-    },
-  },
-
   data() {
     return {
       email: null,
+      error: null,
+      success: false,
+      loading: false,
     }
+  },
+
+  methods: {
+    // onSuccess() {
+    //   this.email = null
+    // },
+
+    onSubmit() {
+      if (this.loading) {
+        return
+      }
+
+      this.loading = true
+
+      axios
+        .post('https://mailing.ueberdosis.io/api/subscribe/b4741e9b-69b7-4ba3-8828-4500236686fe', {
+          email: this.email,
+        })
+        .then(response => {
+          console.log({response})
+          this.email = null
+          this.success = true
+          this.error = null
+          this.loading = false
+        })
+        .catch(error => {
+          console.log({error})
+          this.success = false
+          this.error = error?.response?.data?.message
+          this.loading = false
+        })
+    },
   },
 }
 </script>
