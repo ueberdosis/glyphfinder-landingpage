e9bf84887248bf7e1d326b1d5547b84a12540ef3
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-07 15:11:24 +0100

    fix newsletter form

--- src/components/NewsletterForm/index.vue
+++ src/components/NewsletterForm/index.vue
@@ -1,32 +1,4 @@
 <template>
-  <!-- <mailchimp-subscribe
-    url="https://mailing.mouseless.app/subscribe/post-json"
-    user-id="1bc6d9d915dfd1f9d0950399d"
-    list-id="603ad75049"
-    @success="onSuccess"
-  >
-    <template v-slot="{ subscribe, setEmail, error, success, loading }">
-      <form class="newsletter-form" @submit.prevent="subscribe">
-        <div class="newsletter-form__content">
-          <input
-            class="newsletter-form__input"
-            type="email"
-            v-model="email"
-            @input="setEmail(email)"
-            placeholder="Your email address"
-          >
-          <button class="newsletter-form__button" :disabled="loading" type="submit">
-            Subscribe
-          </button>
-        </div>
-        <div class="newsletter-form__error" v-html="error" v-if="error" />
-        <div class="newsletter-form__success" v-if="success">
-          To complete the subscription process, please click the link in the email we just sent you.
-        </div>
-      </form>
-    </template>
-  </mailchimp-subscribe>
-  <div></div> -->
   <form class="newsletter-form" @submit.prevent="onSubmit">
     <div class="newsletter-form__content">
       <input
@@ -49,14 +21,9 @@
 </template>
 
 <script>
-// import MailchimpSubscribe from 'vue-mailchimp-subscribe'
 import axios from 'axios'
 
 export default {
-  components: {
-    // MailchimpSubscribe,
-  },
-
   data() {
     return {
       email: null,
@@ -67,12 +34,8 @@ export default {
   },
 
   methods: {
-    // onSuccess() {
-    //   this.email = null
-    // },
-
     onSubmit() {
-      if (this.loading) {
+      if (this.loading || !this.email) {
         return
       }
 
@@ -82,17 +45,15 @@ export default {
         .post('https://mailing.ueberdosis.io/api/subscribe/b4741e9b-69b7-4ba3-8828-4500236686fe', {
           email: this.email,
         })
-        .then(response => {
-          console.log({response})
+        .then(() => {
           this.email = null
           this.success = true
           this.error = null
           this.loading = false
         })
         .catch(error => {
-          console.log({error})
           this.success = false
-          this.error = error?.response?.data?.message
+          this.error = error?.response?.data?.errors?.email?.[0] || error?.response?.data?.message
           this.loading = false
         })
     },
