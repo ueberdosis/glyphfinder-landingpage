2f640d6ccf769ad52e50076f597b5d527b067a27
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-10 10:51:28 +0100

    fix section crop

--- src/components/AppSection/index.vue
+++ src/components/AppSection/index.vue
@@ -3,7 +3,7 @@
     :class="['app-section', `app-section--${color}`, { 'is-narrow': narrow }]"
     :style="style"
   >
-    <div class="app-section__background">
+    <div class="app-section__background" :style="backgroundStyle">
       <slot name="background" />
     </div>
     <div class="app-section__inner">
@@ -45,14 +45,19 @@ export default {
 
   computed: {
     style() {
+      return `
+        margin-top: -${this.clipTop ? this.clipOffset : 0}px;
+        padding-top: ${this.clipTop ? (this.clipOffset / 2) : 0}px;
+        padding-bottom: ${this.clipBottom ? (this.clipOffset / 2) : 0}px;
+        `
+    },
+
+    backgroundStyle() {
       const clipPath = `polygon(0 ${this.clipTop ? this.clipOffset : 0}px, 100% 0%, 100% calc(100% - ${this.clipBottom ? this.clipOffset : 0}px), 0% 100%)`
 
       return `
         -webkit-clip-path: ${clipPath};
         clip-path: ${clipPath};
-        margin-top: -${this.clipTop ? this.clipOffset : 0}px;
-        padding-top: ${this.clipTop ? (this.clipOffset / 2) : 0}px;
-        padding-bottom: ${this.clipBottom ? (this.clipOffset / 2) : 0}px;
         `
     },
   },
