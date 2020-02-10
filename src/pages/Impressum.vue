9327fe0f5ff4eeaeb3780615c43a88f9b42c9ad4
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-10 11:04:58 +0100

    add more pages

---
+++ src/pages/Impressum.vue
@@ -0,0 +1,83 @@
+<template>
+  <Layout navigation-theme="bright">
+    <app-section narrow>
+      <h1>
+        Impressum
+      </h1>
+      <p>
+        Information provided according to Sec. 5 German Telemedia Act (TMG)
+      </p>
+      <p>
+        überdosis GbR<br>
+        Kurfürstenstraße 56<br>
+        10785 Berlin<br>
+        Germany
+      </p>
+      <p>
+        Represented by: Sven Adlung, Patrick Baber, Nick Hirche, Philip Isik, Timo Isik, Philipp Kühn, Hans Pagel, Sebastian Schrama
+      </p>
+      <h3>
+        Contact
+      </h3>
+      <p>
+        Email: <a href="mailto:support@mouseless.app">support@mouseless.app</a>
+      </p>
+      <h3>
+        Dispute resolution
+      </h3>
+      <p>
+        The European Commission provides a platform for online dispute resolution (OS): https://ec.europa.eu/consumers/odr.
+      </p>
+      <p>
+        We do not take part in online dispute resolutions at consumer arbitration boards.
+      </p>
+
+      <h3>
+        Liability for Contents
+      </h3>
+      <p>
+        As service providers, we are liable for own contents of these websites according to Sec. 7, paragraph 1 German Telemedia Act (TMG). However, according to Sec. 8 to 10 German Telemedia Act (TMG), service providers are not obligated to permanently monitor submitted or stored information or to search for evidences that indicate illegal activities.
+      </p>
+      <p>
+        Legal obligations to removing information or to blocking the use of information remain unchallenged. In this case, liability is only possible at the time of knowledge about a specific violation of law. Illegal contents will be removed immediately at the time we get knowledge of them.
+      </p>
+
+      <h4>
+        Liability for Links
+      </h4>
+      <p>
+        Our offer includes links to external third party websites. We have no influence on the contents of those websites, therefore we cannot guarantee for those contents. Providers or administrators of linked websites are always responsible for their own contents.
+      </p>
+      <p>
+        The linked websites had been checked for possible violations of law at the time of the establishment of the link. Illegal contents were not detected at the time of the linking. A permanent monitoring of the contents of linked websites cannot be imposed without reasonable indications that there has been a violation of law. Illegal links will be removed immediately at the time we get knowledge of them.
+      </p>
+
+      <h4>
+        Copyright
+      </h4>
+      <p>
+        Contents and compilations published on these websites by the providers are subject to German copyright laws. Reproduction, editing, distribution as well as the use of any kind outside the scope of the copyright law require a written permission of the author or originator. Downloads and copies of these websites are permitted for private use only.
+      </p>
+      <p>
+        The commercial use of our contents without permission of the originator is prohibited.
+      </p>
+      <p>
+        Copyright laws of third parties are respected as long as the contents on these websites do not originate from the provider. Contributions of third parties on this site are indicated as such. However, if you notice any violations of copyright law, please inform us. Such contents will be removed immediately.
+      </p>
+    </app-section>
+  </Layout>
+</template>
+
+<script>
+import AppSection from '@/components/AppSection'
+
+export default {
+  components: {
+    AppSection,
+  },
+
+  metaInfo: {
+    title: 'Impressum',
+  },
+}
+</script>
---
+++ src/pages/Privacy.vue
@@ -0,0 +1,83 @@
+<template>
+  <Layout navigation-theme="bright">
+    <app-section narrow>
+      <h1>
+        Privacy Policy for the Website
+      </h1>
+      <p>
+        We value your privacy and question everything that stores or processes your personal information. With that in mind, we still needed a few external services to publish Mouseless. Here is everything you need to know.
+      </p>
+      <h3>
+        Netlify
+      </h3>
+      <p>
+        We use Netlify to host our static website. It features continuous deployment from Git across a global application delivery network and full integration with Let’s Encrypt. <a href="https://www.netlify.com/privacy/">Read Netlify’s Privacy Policy.</a>
+      </p>
+      <h3>
+        Gumroad
+      </h3>
+      <p>
+        Gumroad is an online platform that enables us to sell Mouseless directly to you. <a href="https://gumroad.com/privacy">Read Gumroad’s Privacy Policy.</a>
+      </p>
+      <h3>
+        Mailchimp
+      </h3>
+      <p>
+        We use Mailchimp to send out emails to you. However, we are about to switch to a self-hosted solution.
+      </p>
+      <h3>
+        Simple Analytics
+      </h3>
+      <p>
+        To get critical information about the behavior of our visitors, we use <a href="https://simpleanalytics.com" target="_blank">Simple Analytics</a>. This analytics software gives us insight about our visitors only in general, but not about individuals per say, as it does not track visitors and does not store any personal identifiable information. <a href="https://docs.simpleanalytics.com/what-we-collect" target="_blank">Go to their documentation</a> to find out what Simple Analytics collects (and most importantly what they don't).
+      </p>
+      <p>
+        Or have a look at our analytics dashboard: <a href="https://simpleanalytics.com/mouseless.app">https://simpleanalytics.com/mouseless.app</a>
+      </p>
+      <h3>
+        Floatie
+      </h3>
+      <p>
+        We built a service that aggregates data from all those services (SimpleAnalytics, Mailchimp, Gumroad) and shows a public dashboard with all the metrics: <a href="https://floatie.app/mouseless">https://floatie.app/mouseless</a>. It does not store any personal information.
+      </p>
+      <h1>
+        Privacy Policy for Mouseless
+      </h1>
+      <p>
+        We don’t track your usage of Mouseless. A few points may need some explanation though:
+      </p>
+      <h3>
+        License Check
+      </h3>
+      <p>
+        When you start the app for the first time we ask you for your license key. To check if you have a valid license key, we send the key to the Gumroad API. The result is stored locally and never sent to someone else. <a href="https://help.gumroad.com/article/76-license-keys">Read more about the Gumroad API.</a>
+      </p>
+      <h3>
+        Screen Recording Permission
+      </h3>
+      <p>
+        Mouseless asks you to give the Screen Recording Permission. We don’t use this to actually record your screen, but to check which app is currently running to show you the relevant keyboard shortcuts in the menu bar. <a href="https://github.com/ueberdosis/window-shortcuts">Have a look at our code on GitHub.</a>
+      </p>
+      <h3>
+        Accessibility Permission
+      </h3>
+      <p>
+        Mouseless asks you to give the Accessibility Permission. We need this permission to register a global keyboard shortcut to open Mouseless.
+      </p>
+    </app-section>
+  </Layout>
+</template>
+
+<script>
+import AppSection from '@/components/AppSection'
+
+export default {
+  components: {
+    AppSection,
+  },
+
+  metaInfo: {
+    title: 'Privacy Policy',
+  },
+}
+</script>
