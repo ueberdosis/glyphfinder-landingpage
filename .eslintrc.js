37c9ced73154c5a0c4f99ad083e5754689ef61ca
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-10 09:44:15 +0100

    disable eslint max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  'extends': [
+  extends: [
     'scrumpy',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
