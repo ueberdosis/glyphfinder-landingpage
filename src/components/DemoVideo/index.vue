c951fab18422cb7efc76c7073f6519358fa2783a
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-06 09:24:05 +0100

    only play video when in viewport

--- src/components/DemoVideo/index.vue
+++ src/components/DemoVideo/index.vue
@@ -2,10 +2,10 @@
   <div class="demo-video">
     <div class="demo-video__ratio" :style="`padding-bottom: ${ratio * 100}%`">
       <video
+        ref="video"
         class="demo-video__video"
         :src="url"
         playsinline
-        autoplay
         muted
         loop
       />
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import scrollMonitor from 'scrollmonitor'
+
 export default {
   props: {
     ratio: {
@@ -27,10 +29,57 @@ export default {
     },
   },
 
+  data() {
+    return {
+      view: null,
+    }
+  },
+
   computed: {
     url() {
       return require(`~/assets/videos/${this.name}`)
     },
+  },
+
+  methods: {
+    play() {
+      const { video } = this.$refs
+
+      if (video && !this.isPlaying()) {
+        video.play()
+      }
+    },
+
+    pause() {
+      const { video } = this.$refs
+
+      if (video && this.isPlaying()) {
+        video.pause()
+      }
+    },
+
+
+    isPlaying() {
+      const { video } = this.$refs
+
+      return video
+        && video.currentTime > 0
+        && !video.paused
+        && !video.ended
+        && video.readyState > 2
+    },
+  },
+
+  mounted() {
+    this.view = scrollMonitor.create(this.$el, { top: 50, bottom: 50 })
+    this.view.enterViewport(this.play)
+    this.view.exitViewport(this.pause)
+  },
+
+  beforeDestroy() {
+    if (this.view) {
+      this.view.destroy()
+    }
   },
 }
 </script>
