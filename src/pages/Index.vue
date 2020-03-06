436afd0f435d2893f993366c3277547e119405e1
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-06 09:30:09 +0100

    improve grid

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -38,17 +38,17 @@
 
     <app-section color="black">
       <grid :options="{ default: 'wide'}">
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <feature-item title="Looks like …" video="lookalikes.mp4">
             It’s easier to visually remember a glyph, so we made it easy to search for look-alikes. Type anything you remember and find the correct glyph instantly.
           </feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <feature-item title="The Best Emoji Search" video="emojis.mp4">
             We have manually added and tagged every single emoji. That probably makes Glyphfinder the single best emoji search out there. 🧐
           </feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <feature-item title="Learn More" video="shortcuts.mp4">
             In case you want to go mouseless, learn the keyboard shortcuts for all your favorite glyphs right away. Coders copy the entity.
           </feature-item>
@@ -58,32 +58,32 @@
 
     <app-section color="deep-black">
       <grid :options="{ default: 'wide'}">
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <small-feature-item title="30.000+ Glyphs" icon="glyphs">
             With our enormous database you have more than 30.000 characters right at your fingertips, every single one manually tagged.
           </small-feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <small-feature-item title="Quick Access" icon="frequently-used">
             Don‘t feel ashamed looking up characters multiple times. Your most frequently used glyphs will be pinned to the top.
           </small-feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <small-feature-item title="Offline Support" icon="offline">
             No Wi-fi – no problem. Glyphfinder runs just as smoothly in a rustic cabin as on a plane.
           </small-feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <small-feature-item title="Really Fast Search" icon="fast">
             Search should feel instant, even when looking through hundred of thousands of tags, so that‘s what we made it.
           </small-feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <small-feature-item title="Dark Mode by Default" icon="dark-mode">
             No one wants a flash of bright light in their face when looking up a few glyphs. Enjoy the dark theme for Glyphfinder, it’s the only theme anyway.
           </small-feature-item>
         </grid-item>
-        <grid-item :options="{ medium: '4/12'}">
+        <grid-item :options="{ small: '6/12', medium: '4/12'}">
           <small-feature-item title="macOS & Windows" icon="mac">
             Glyphfinder will work on your favorite operating system (as long as it’s not Linux). All licenses include both apps.
           </small-feature-item>
